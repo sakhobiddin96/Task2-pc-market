08237d13d6554daeed00b44a45ac0a429c437c7c
sakhobiddin <nsakhobiddin@gmail.com>
2022-05-12 13:38:56 +0200

    fourth commit

--- src/main/java/uz/pdp/task2/repository/ComputersRepository.java
+++
@@ -1,9 +0,0 @@
-package uz.pdp.task2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import uz.pdp.task2.entity.Computers;
-
-@RepositoryRestResource(path = "computers")
-public interface ComputersRepository extends JpaRepository<Computers,Integer> {
-}
--- src/main/java/uz/pdp/task2/repository/accessories/CoolingSystemsRepository.java
+++
@@ -1,8 +0,0 @@
-package uz.pdp.task2.repository.accessories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import uz.pdp.task2.entity.accessories.CoolingSystems;
-@RepositoryRestResource(path = "coolingSystems")
-public interface CoolingSystemsRepository extends JpaRepository<CoolingSystems,Integer> {
-}
